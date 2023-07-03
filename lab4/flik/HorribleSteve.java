4d8661000f5adda0fccfa46c17ac7b346e072fe9
mengtaoli <2741847511@qq.com>
2023-07-03 15:50:22 +0800

    lab4 debug exercise

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -1,10 +1,12 @@
 package flik;
 
 public class HorribleSteve {
-    public static void main(String [] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         int i = 0;
+        boolean result;
         for (int j = 0; i < 500; ++i, ++j) {
-            if (!Flik.isSameNumber(i, j)) {
+            result = Flik.isSameNumber(i, j);
+            if (!result) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
             }
