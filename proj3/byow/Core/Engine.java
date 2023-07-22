84b387d13f476bdd0aa013d95b686e0b85c84c6f
mengtaoli <2741847511@qq.com>
2023-07-22 14:22:15 +0800

    project 3 phase 1 seems work

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -27,6 +27,8 @@ public class Engine {
 
     private TreeSet<Room> existingRooms = new TreeSet<>();
 
+    private enum DIRECTION {LEFT_ABOVE, RIGHT_ABOVE, LEFT_BELOW, RIGHT_BELOW};
+
 
 
     private class Position {
@@ -128,6 +130,11 @@ public class Engine {
             return  Math.pow((this.shiftX(this.width / 2) - room.shiftX(room.width / 2)), 2)
                     + Math.pow((this.shiftY(this.length / 2)- room.shiftY(room.length / 2)), 2);
         }
+
+        public Position middlePoint() {
+            Position p = new Position(this.shiftX(this.width / 2), this.shiftY(this.length / 2));
+            return p;
+        }
     }
     /**
      * Method used for exploring a fresh world. This method should handle all inputs,
@@ -170,16 +177,12 @@ public class Engine {
         Engine.fillOut(finalWorldFrame);
 
         // get seed from input string, for now, input just seed
-        String seed = input;
+        String seed = input.substring(1, input.length() - 1) ;
+        System.out.println(seed);
         random = new Random(Long.parseLong(seed));
 
         drawRooms(finalWorldFrame);
         drawHallways(finalWorldFrame);
-        /*
-        Position start = new Position(1, 0);
-        Position end = new Position(1,6);
-        drawVerticalHallWay(finalWorldFrame, start, end);
-         */
         return finalWorldFrame;
     }
 
@@ -192,11 +195,14 @@ public class Engine {
         Room currentRoom = existingRooms.first();
         Room closest;
         while (!existingRooms.isEmpty()) {
+            if (currentRoom.position.x == 27 || currentRoom.position.x == 28) {
+                //world[currentRoom.position.x][currentRoom.position.y] = Tileset.FLOWER;
+            }
             existingRooms.remove(currentRoom);
             if (!existingRooms.isEmpty()) {
                 closest = connectWithClosestRoom(world, currentRoom);
-                System.out.println(currentRoom.position);
-                System.out.println(closest.position);
+                //System.out.println(currentRoom.position);
+                //System.out.println(closest.position);
                 currentRoom = closest;
             }
         }
@@ -223,7 +229,7 @@ public class Engine {
                 closestDistance = r.distanceTo(currentRoom);
             }
         }
-        System.out.println(closestDistance);
+        //System.out.println(closestDistance);
         return closest;
     }
 
@@ -234,12 +240,112 @@ public class Engine {
     private void connectWithClosestRoom_Helper(TETile[][] world, Room currentRoom, Room closest) {
         if (verticalHallways(world, currentRoom, closest)) {
             return;
-        }
-        if (horizontalHallways(world, currentRoom, closest)) {
+        } else if (horizontalHallways(world, currentRoom, closest)) {
             return;
+        } else {
+            L_shape_Hallway(world, currentRoom, closest);
+        }
+
+    }
+
+    private void L_shape_Hallway(TETile[][] world, Room currentRoom, Room closest) {
+        Position currentMiddle = currentRoom.middlePoint();
+        Position closestMiddle = closest.middlePoint();
+        // current middle point is on the left above direction of the closest middle point
+        if (left_above(currentMiddle, closestMiddle)) {
+            //System.out.println("left above");
+            draw_L_shape(world, currentMiddle, closestMiddle, DIRECTION.LEFT_ABOVE);
+        } else if (right_above(currentMiddle, closestMiddle)) {
+            //System.out.println("right above");
+            draw_L_shape(world, currentMiddle, closestMiddle, DIRECTION.RIGHT_ABOVE);
+        } else if (left_below(currentMiddle, closestMiddle)) {
+           // System.out.println("left below");
+            draw_L_shape(world, closestMiddle, currentMiddle, DIRECTION.RIGHT_ABOVE);
+        } else if (right_below(currentMiddle, closestMiddle)) {
+            //System.out.println("right below");
+            draw_L_shape(world, closestMiddle, currentMiddle, DIRECTION.LEFT_ABOVE);
+        }
+
+    }
+
+    private void draw_L_shape(TETile[][] world, Position currentMiddle, Position closestMiddle, DIRECTION direction) {
+        Position start, end;
+
+        if (direction == DIRECTION.LEFT_ABOVE) {
+
+            start = new Position(currentMiddle.x + 1, currentMiddle.y - 1);
+            end = new Position(closestMiddle.x - 1, closestMiddle.y + 1);
+            draw_L_Shape(world, start, end, Tileset.WALL, DIRECTION.LEFT_ABOVE);
+
+            start = new Position(currentMiddle.x + 1, currentMiddle.y);
+            end = new Position(closestMiddle.x, closestMiddle.y + 1);
+            draw_L_Shape(world, start, end, Tileset.FLOOR, DIRECTION.LEFT_ABOVE);
+
+            start = new Position(currentMiddle.x + 1, currentMiddle.y + 1);
+            end = new Position(closestMiddle.x + 1, closestMiddle.y + 1);
+            draw_L_Shape(world, start, end, Tileset.WALL, DIRECTION.LEFT_ABOVE);
+
+        } else if (direction == DIRECTION.RIGHT_ABOVE) {
+            start = new Position(closestMiddle.x + 1, closestMiddle.y + 1);
+            end = new Position(currentMiddle.x - 1, currentMiddle.y - 1);
+            draw_L_Shape(world, start, end, Tileset.WALL, DIRECTION.RIGHT_ABOVE);
+
+            start = new Position(closestMiddle.x, closestMiddle.y + 1);
+            end = new Position(currentMiddle.x - 1, currentMiddle.y);
+            draw_L_Shape(world, start, end, Tileset.FLOOR, DIRECTION.RIGHT_ABOVE);
+
+            start = new Position(closestMiddle.x - 1, closestMiddle.y + 1);
+            end = new Position(currentMiddle.x - 1, currentMiddle.y + 1);
+            draw_L_Shape(world, start, end, Tileset.WALL, DIRECTION.RIGHT_ABOVE);
         }
     }
 
+    private void draw_L_Shape(TETile[][] world, Position start, Position end, TETile tile, DIRECTION direction) {
+        if (direction == DIRECTION.LEFT_ABOVE) {
+            Position horizontal_end = new Position(end.x + 1,  start.y);
+            draw_L_Shape_Horizontal(world, start, horizontal_end, tile);
+
+            Position vertical_start = end;
+            Position vertical_end = new Position(end.x, start.y + 1);
+            draw_L_Shape_Vertical(world, vertical_start, vertical_end, tile);
+        } else if (direction == DIRECTION.RIGHT_ABOVE) {
+            Position vertical_end = new Position(start.x, end.y + 1);
+            draw_L_Shape_Vertical(world, start, vertical_end, tile);
+
+            Position horizontal_start = new Position(start.x, end.y);
+            draw_L_Shape_Horizontal(world, horizontal_start, end, tile);
+        }
+    }
+    private void draw_L_Shape_Horizontal(TETile[][] world, Position start, Position end, TETile tile) {
+        int y = start.y;
+        for (int i = start.x; i < end.x; i++) {
+            if (world[i][y].equals(Tileset.FLOOR)) {
+                continue;
+            }
+            world[i][y] = tile;
+        }
+    }
+    private void draw_L_Shape_Vertical(TETile[][] world, Position start, Position end, TETile tile) {
+        int x = start.x;
+        for (int i = start.y; i < end.y; i++) {
+            if (world[x][i].equals(Tileset.FLOOR)) {
+                continue;
+            }
+            world[x][i] = tile;
+        }
+    }
+    private boolean left_above(Position current, Position closest) {
+        return current.x < closest.x && current.y > closest.y;
+    }
+    private boolean right_above(Position current, Position closest) {
+        return current.x > closest.x && current.y > closest.y;
+    }
+    private boolean left_below(Position current, Position closest) {
+        return current.x < closest.x && current.y < closest.y;
+    }
+    private boolean right_below(Position current, Position closest) {
+        return current.x > closest.x && current.y < closest.y;
+    }
     private boolean horizontalHallways(TETile[][] world, Room currentRoom, Room closest) {
         int currentYPosition = currentRoom.shiftY(currentRoom.length / 2);
         if (withinBounds(currentYPosition, closest.shiftY(0), closest.shiftY(closest.length - 1))) {
@@ -255,7 +361,7 @@ public class Engine {
     }
 
     private void horizontalHallways_Helper(TETile[][] world, Room currentRoom, Room closest, int yPosition) {
-        System.out.println("in horizontal hallway helper");
+        //System.out.println("in horizontal hallway helper");
         // closest is in the left of the current room
         if (closest.shiftX(closest.width - 1) < currentRoom.shiftX(0)) {
             //System.out.println("closest is on the left of the current room");
@@ -273,9 +379,13 @@ public class Engine {
     private void drawHorizontalHallway(TETile[][] world, Position start, Position end) {
         int yPosition = start.y;
         for (int i = start.x; i <= end.x; i++) {
-            world[i][yPosition - 1] = Tileset.WALL;
+            if (!world[i][yPosition - 1].equals(Tileset.FLOOR)) {
+                world[i][yPosition - 1] = Tileset.WALL;
+            }
             world[i][yPosition] = Tileset.FLOOR;
-            world[i][yPosition + 1] = Tileset.WALL;
+            if (!world[i][yPosition + 1].equals(Tileset.FLOOR)) {
+                world[i][yPosition + 1] = Tileset.WALL;
+            }
         }
     }
     private boolean verticalHallways(TETile[][] world, Room currentRoom, Room closest) {
@@ -292,7 +402,7 @@ public class Engine {
         return false;
     }
     private void verticalHallway_Helper(TETile[][] world, Room currentRoom, Room closest, int xPosition) {
-        System.out.println("in vertical hallway helper");
+        //System.out.println("in vertical hallway helper");
         if (closest.shiftY(closest.length) <= currentRoom.shiftY(0) ) {
             Position start = new Position(xPosition, closest.shiftY(closest.length - 1));
             Position end = new Position(xPosition, currentRoom.shiftY(0));
@@ -306,9 +416,13 @@ public class Engine {
     private void drawVerticalHallWay(TETile[][] world, Position start, Position end) {
         int xPosition = start.x;
         for (int i = start.y; i <= end.y; i++) {
-            world[xPosition - 1][i] = Tileset.WALL;
+            if (!world[xPosition - 1][i].equals(Tileset.FLOOR)) {
+                world[xPosition - 1][i] = Tileset.WALL;
+            }
             world[xPosition][i] = Tileset.FLOOR;
-            world[xPosition + 1][i] = Tileset.WALL;
+            if (!world[xPosition + 1][i].equals(Tileset.FLOOR)) {
+                world[xPosition + 1][i] = Tileset.WALL;
+            }
         }
     }
 
@@ -327,7 +441,7 @@ public class Engine {
 
     private void drawRooms(TETile[][] finalWorldFrame) {
         int x = random.nextInt(ROOM_NUMBER_MIN, ROOM_NUMBER_MAX);
-        System.out.println("number of room : " + x);
+        //System.out.println("number of room : " + x);
         for (int i = 0; i < x; i++) {
             drawRandomRoom(finalWorldFrame);
         }
@@ -398,7 +512,7 @@ public class Engine {
         TERenderer ter = new TERenderer();
         ter.initialize(WIDTH, HEIGHT);
         Engine engine = new Engine();
-        String seed = "123";
+        String seed = "n123s";
         TETile[][] world = engine.interactWithInputString(seed);
         ter.renderFrame(world);
 
